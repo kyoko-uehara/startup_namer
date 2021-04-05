c97ce1f43912bf942815fc4c1d41ac402c345ee6
kyoko-uehara <kyoko.uehara@stv-tech.co.jp>
2021-04-05 11:44:12 +0900

    Step 3: Add a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() =>runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    //-final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to Flutter APP',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Welcome to Flutter appBar'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          //-child: Text('Hello World'),
+          //-child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
+
     );
+    throw UnimplementedError();
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
